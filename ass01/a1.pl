d3d9979ef583be0d48dda3b720b51cfac02445a2
Ryan Brooks <dootfiddle@Ryan-Brookss-MacBook-Pro.local>
2014-01-20 17:52:02 -0600

    steak

--- ass01/a1.pl
+++ ass01/a1.pl
@@ -1,3 +1,3 @@
 #! /usr/bin/perl
 
-print "\nbash: ./a1.pl: Permission denied\n\n";
+print "\n\n\t\tIngredients\n-6 slices Filet Mignon, each about 1/2 pound and cut about 1 1/2 inches thickSalt and pepper\n-1/3 cup (6 tablespoons) butter\n-1 1/2 tablespoons olive oil\n\n\t\t Directions\nSeason the Filet Mignons with salt and pepper.\nCombine the butter and oil in a large saute pan over high heat.\nWhen the pan is hot and foam has subsided, add the beef.\nSear for three minutes per side for rare.\nTransfer to a warm platter.\n\n";

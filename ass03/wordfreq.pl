8a0803e10910d895c07df03833515766bd5a8428
Ryan <rbrooks@cs.utexas.edu>
2014-02-05 10:17:40 -0600

    ass03 finished

--- ass03/wordfreq.pl
+++ ass03/wordfreq.pl
@@ -3,63 +3,34 @@
 use v5.10;
 
 $debug = 0;
-
 open(WORDS, "<", "/usr/share/dict/words");
 
+#compiling dictionary words
 @dictWords;
-while(<WORDS>)
-{
+while(<WORDS>){
 	chomp;
-	push(@dictWords, map { lc } $_);
-}
+	push(@dictWords, $_);}
 
-if ($debug > 1)
-{
-	for( @dictWords)
-	{
-		print "$_\n";
-	}
-} 
-
-%wordCount;
-
-while(<>)
-{
+#reading in words to count
+while(<>){
 	@words_in_line = /[a-z](?:[a-z']*[a-z])?/ig;
+	foreach(@words_in_line) { $wordCount{lc $_}++; }}
 
-	foreach(@words_in_line)
-	{
-		$wordCount{lc $_}++;
-	}
-	
-}
-
-if($debug)
-{
-	foreach my $key (keys %wordCount) 
-	{
+if($debug) {
+	for( @dictWords) { print "$_\n"; }
+	foreach my $key (keys %wordCount) {
 		$v = $wordCount{$key};
-		print "$key : $v\n";
-	} 
-}
+		print "$key : $v\n";}}
 
-print "\n\nsorting\n\n\n" if ($debug);
+#sorting all words
 @sortedWords = sort { $wordCount{$b} <=> $wordCount{$a} or $a cmp $b} keys %wordCount; 
+printf "Number of distinct words: %i\n",scalar(@sortedWords);
+for($i= 0; $i < 10; $i++) { printf "$sortedWords[$i]: %i,\n", $wordCount{$sortedWords[$i]}; }
 
-for( $i= 0; $i < 10; $i++)
-{
-	printf "$sortedWords[$i]: %i,\n", $wordCount{$sortedWords[$i]};
-}
+#deleting common words
+for (@dictWords) { delete $wordCount{$_}; }
 
-
-for (@dictWords)
-{
-	delete $wordCount{$_};
-}
-print "\n";
-print "\n\n\n\nuncommon \n\n\n\n\n" if ($debug);
+#sorting uncommon words
 @sortedWords = sort {$wordCount{$b} <=> $wordCount{$a} or $a cmp $b} keys (%wordCount); 
-for( $i= 0; $i < 10; $i++)
-{	
-	printf "$sortedWords[$i]: %i,\n", $wordCount{$sortedWords[$i]}; 
-}
+printf "Number of distinct words not in dictionary: %i\n",scalar(@sortedWords);
+for($i= 0; $i < 10; $i++) {	printf "$sortedWords[$i]: %i\n", $wordCount{$sortedWords[$i]}; }

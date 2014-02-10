5cb621cfb1df766c75c7b8d6b39770fefc2d48bf
Ryan <rbrooks@cs.utexas.edu>
2014-02-10 17:30:06 -0600

    turned in

--- ass04/calc.pl
+++ ass04/calc.pl
@@ -1,6 +1,5 @@
 #! /usr/bin/perl
 use v5.10;
-
 our @stack;
 
 sub sqrt{
@@ -9,37 +8,27 @@ sub sqrt{
 }
 
 sub sum {
-	# body...
     $stackSum = &calcSum();
 	undef our @stack;
 	push our @stack, $stackSum;
 }
 
 sub mean {
-	# body...
-	#Replace the stack with the arithmetic mean of all the numbers that were on the stack. This value should 
-	#be equal to the sum of all the numbers divided by the size of the stack.
 	$stackMean = &calcMean();
 	undef our @stack;
 	push our @stack, $stackMean; 
 }
 
 sub squares{
-	#Multiply each number on the stack by itself.
 	for(our @stack) { $_ *= $_; }
 }
 
 sub residuals {
-	# body...
-	#Subtract from each number on the stack the mean of all the numbers on the stack.
 	$mean = &calcMean();
 	for(our @stack){ $_ -= $mean }
 }
 
 sub stddev {
-	#This value should be equal to the square root of
-	# the mean of the squares of the residuals of all the numbers on the stack.
-	#sqrt ( mean ( squares (residuals)) )
 	&residuals();
 	&squares();
 	&mean();
@@ -59,10 +48,6 @@ sub calcMean {
 while(<>)
 {
 	chomp;
-    
-	#do some reading and printing still
-    print ("pre Stack: @stack\n");
-    
 	given($_) {
 		when ('+') {$right = pop our @stack;
                     $left = pop our @stack ;
@@ -88,5 +73,4 @@ while(<>)
 		when ('stddev') {&stddev();  print ("Stack: @stack\n");}
 		default { push our @stack, $_} #push operand
 	}
-    print ("post Stack: @stack\n");
 }

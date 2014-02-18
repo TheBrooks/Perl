99b3aeb40583856bc001e3cc91ee02b62563e00e
Ryan <rbrooks@cs.utexas.edu>
2014-02-17 21:20:51 -0600

    regex needs a fix on the trailing side checking that nothing happens after the fact

---
+++ ass05/email.pl
@@ -0,0 +1,18 @@
+#! /usr/bin/perl
+use v5.10;
+
+$debug = 0;
+
+while(<>){
+	@emails_in_line = /(?<!\S)[a-z0-9-_]+(?:\.[a-z0-9-_]+)?@[a-z0-9-_]+(?:\.[a-z0-9-_]+)?\b/igx;
+	foreach(@emails_in_line) 
+	{ 
+		$emailCount{$_}++; 
+	}
+	#print("reg @emails_in_line line $_\n");
+}
+
+#sorting all words
+@sortedEmails = sort { $emailCount{$b} <=> $emailCount{$a} or $a cmp $b} keys %emailCount; 
+printf "Number of distinct words: %i\n",scalar(@sortedEmails);
+for($i= 0; $i < scalar(@sortedEmails); $i++) { printf "$sortedEmails[$i]: (%i)\n", $emailCount{$sortedEmails[$i]}; }
---
+++ ass05/testEmails.pl
@@ -0,0 +1,4 @@
+
+
+good.emails@are.below.down
+abc.1234.asdfg@qwertyuio.asdfg

6bc338c54058b0c8471a2080d2f8c748bde392e3
TheBrooks <dootfiddle@Ryan-Brookss-MacBook-Pro.local>
2014-02-24 20:57:40 -0600

    modified code

--- ass05/email.pl
+++ ass05/email.pl
@@ -9,10 +9,8 @@ while(<>){
 	{ 
 		$emailCount{$_}++; 
 	}
-	#print("reg @emails_in_line line $_\n");
 }
 
-#sorting all words
 @sortedEmails = sort { $emailCount{$b} <=> $emailCount{$a} or $a cmp $b} keys %emailCount; 
 printf "Number of distinct words: %i\n",scalar(@sortedEmails);
 for($i= 0; $i < scalar(@sortedEmails); $i++) { printf "$sortedEmails[$i]: (%i)\n", $emailCount{$sortedEmails[$i]}; }

b7617c0712ca51eabbed1c1d10509786f3996276
TheBrooks <dootfiddle@Ryan-Brookss-MacBook-Pro.local>
2014-02-25 12:58:48 -0600

    perl classes are hard

--- ass06/matrix.pm
+++ ass06/matrix.pm
@@ -10,26 +10,54 @@ use strict;
 use warnings;
 
  use overload
-	'+' => \&myadd,
-	'-' => \&mysub;
-	'*' => \&mymulti
+	'+' => 'myadd',
+	'-' => 'mysub',
+	'*' => 'mymulti';
 
 sub new {
     my $class = shift;
     my $self  = [];
+    for($row (@_))
+    	$self->&add_row(@$row);
     return bless  $self, $class;
 }
 
 sub add_row {
 	my $self = shift;
-	@matrix = $self;
-	@newRow = split(" ",pop(@_));
-	push (@matrix , @newRow);
+	@newRow = split(" ",shift);
+	push (@{$self} , @newRow);
+}
+
+sub getHeight {
+	my $self = shift;
+	return scalar(@{$self});
+}
+
+sub getWidth {
+	my $self = shift;
+	return scalar(@{$self->[0]})
 }
 
 sub myadd {
 	my $self = shift;
+	my $rhs = shift;
+	my $returnMatrix = $self->new();
+
+	#need to get width and height
+	my $height = $self->getHeight();
+	my $width = $self->getWidth();
+
+	for my $i(0..($height - 1)){
+		$newRow = "";
+		for my $j (0..($width - 1)){
+			$newIndex = $self->[i][j] + $rhs->[i][j]
+			$newRow .= " $newIndex ";
+		}
+		$returnMatrix -> add_row($newRow)
+	}
+	return $returnMatrix;
 }
+
 
 sub mysub {
 	my $self = shift;
